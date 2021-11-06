79ddbe076854a98ec67a633863b8408ea81e3760
kai qi <kqi3@uic.edu>
2021-11-06 18:29:18 -0500

    add unique constraints that not PK in store,employee,tb_user

--- Documentation/create_tables_donation.sql
+++ Documentation/create_tables_donation.sql
@@ -5,7 +5,8 @@ CREATE TABLE `store` (
   `phone` varchar(20) NOT NULL,
   `email` varchar(50) NOT NULL,
   PRIMARY KEY (`store_id`),
-  UNIQUE KEY `store_id_UNIQUE` (`store_id`)
+  UNIQUE KEY `store_id_UNIQUE` (`store_id`),
+  UNIQUE KEY `store_name_UNIQUE` (`store_name`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `employee` (
   `employee_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -17,6 +18,7 @@ CREATE TABLE `employee` (
   `email` varchar(50) NOT NULL,
   PRIMARY KEY (`employee_id`),
   UNIQUE KEY `employee_id_UNIQUE` (`employee_id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
   KEY `employee` (`store_id`),
   CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`store_id`) REFERENCES `store` (`store_id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -29,15 +31,8 @@ CREATE TABLE `tb_user` (
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   PRIMARY KEY (`user_id`),
-  UNIQUE KEY `user_id_UNIQUE` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `donation_cart` (
-  `cart_id` int(11) NOT NULL,
-  `donator_id` int(11) NOT NULL DEFAULT '0',
-  `date` date NOT NULL,
-  PRIMARY KEY (`cart_id`),
-  UNIQUE KEY `donator_id` (`donator_id`),
-  CONSTRAINT `donation_cart_ibfk_1` FOREIGN KEY (`donator_id`) REFERENCES `tb_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  UNIQUE KEY `user_id_UNIQUE` (`user_id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -53,7 +48,15 @@ CREATE TABLE `orders` (
   CONSTRAINT `fk_order_user` FOREIGN KEY (`user_id`) REFERENCES `tb_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `sales_employee` FOREIGN KEY (`sales_id`) REFERENCES `employee` (`employee_id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+CREATE TABLE `donation_cart` (
+  `cart_id` int(11) NOT NULL AUTO_INCREMENT,
+  `donator_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`cart_id`),
+  UNIQUE KEY `cart_id_UNIQUE` (`cart_id`),
+  KEY `fk_cart_user_idx` (`donator_id`),
+  CONSTRAINT `fk_cart_user` FOREIGN KEY (`donator_id`) REFERENCES `tb_user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `category` (
   `category_id` int(11) NOT NULL AUTO_INCREMENT,
   `category_name` varchar(50) NOT NULL,
@@ -61,6 +64,7 @@ CREATE TABLE `category` (
   PRIMARY KEY (`category_id`),
   UNIQUE KEY `category_id_UNIQUE` (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `item` (
   `item_id` int(11) NOT NULL AUTO_INCREMENT,
   `order_id` int(11) DEFAULT '0',
@@ -72,11 +76,12 @@ CREATE TABLE `item` (
   UNIQUE KEY `item_id` (`item_id`),
   KEY `order_id` (`order_id`),
   KEY `cart_id` (`cart_id`),
+  KEY `item_ibfk_2` (`category_id`),
   CONSTRAINT `item_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `item_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `item_ibfk_3` FOREIGN KEY (`cart_id`) REFERENCES `donation_cart` (`cart_id`) ON DELETE SET NULL ON UPDATE CASCADE
-  
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `user` (
   `username` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
