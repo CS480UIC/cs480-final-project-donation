ec760c698f05ca771fe3727a0f27a05c0177756c
kai qi <kqi3@uic.edu>
2021-11-25 19:35:36 -0600

    store create and store read

--- Documentation/category_insert.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO donation.category (`category_id`,`category_name`,`last_update`) VALUES (1101,'Books','2021-10-19');
-INSERT INTO donation.category (`category_id`,`category_name`,`last_update`) VALUES (1102,'Toys','2021-10-18');
-INSERT INTO donation.category (`category_id`,`category_name`,`last_update`) VALUES (1103,'electronics','2021-10-16');
-INSERT INTO donation.category (`category_id`,`category_name`,`last_update`) VALUES (1104,'clothes','2021-10-14');
-INSERT INTO donation.category (`category_id`,`category_name`,`last_update`) VALUES (1105,'Furniture','2021-10-10');
-INSERT INTO donation.category (`category_id`,`category_name`,`last_update`) VALUES (1106,'Tools','2021-10-06');
